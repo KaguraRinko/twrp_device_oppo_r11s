e6969be2852c1f19cc6edac01ebb1f603f715d0b
baolong24 <1667375308@qq.com>
2021-01-31 11:57:47 +0800

    r11s: init tree

---
+++ omni_r11s.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Release name
+PRODUCT_RELEASE_NAME := wayne
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_NAME := omni_r11s
+PRODUCT_DEVICE := r11s
+PRODUCT_MANUFACTURER := OPPO
+PRODUCT_BRAND := OPPO
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+TARGET_VENDOR := OPPO
+TARGET_VENDOR_PRODUCT_NAME := r11s
+TARGET_VENDOR_DEVICE_NAME := r11s
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=r11s PRODUCT_NAME=r11s
